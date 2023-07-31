ce6c03149f13fe826c965e285a3b930964c8d524
djibril soukouna <djib_cisse_9@hotmail.com>
2023-07-31 13:50:32 +0200

    ajout modal mdp oublié

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,31 @@
 import styles from '@styles/Home.module.scss';
+import { useState } from 'react';
 import { Row, Col, Card, Input, Button, Space, Modal } from 'antd';
 
 function Home() {
+  const [modal, setModal] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const mdpOublie = () => {
+    return <>
+    </>
+  }
+
+  const creationCompte = () => {
+    return <>
+    Lahrim
+    </>
+  }
+
+  const clickModalOpen = (data) => {
+    setModal(true)
+    setModalOpen(data)
+  }
+
+  const réinitialisationMDP = () => {
+    setModal(false)
+  }
+
   return (
     <>
       <main>
@@ -19,11 +43,11 @@ function Home() {
                     <Input placeholder='Email' size='large'/>
                     <Input placeholder='Mot de passe' size='large'/>
                     <Button type='default' size='large'>Se connecter</Button>
-                    <Button type='link'>Mot de passe oublié ?</Button>
+                    <Button type='link' onClick={ () => clickModalOpen(true)}>Mot de passe oublié ?</Button>
                   </Space>
                   
                   <Space direction='vertical' className='w-100 text-center'>
-                    <Button type='success' size='large'>Créer un compte</Button>
+                    <Button type='success' size='large' onClick={ () => clickModalOpen(false)}>Créer un compte</Button>
                   </Space>
                 </Space>
               </Card>
@@ -34,6 +58,31 @@ function Home() {
        <section className='section-'></section>
        <section className='section-'></section>
       </main>
+      
+        <Modal footer={null} centered open={modal} onCancel={() => setModal(false)} wrapClassName={styles.modal } 
+        title="Réinitialisation mot de passe"
+        >
+        <Space direction='vertical' className='w-100 text-center' size={12}>
+          <p>Veuillez renseigner votre adresse e-mail pour réinitialiser votre mot de passe</p>
+          <Input placeholder='Email'></Input>
+          <Button type='default' size='large' onClick={ () => réinitialisationMDP()} >Valider</Button>
+          { modalOpen ? mdpOublie() : creationCompte() }
+        </Space>
+        </Modal>
+
+        {/* <Modal footer={null} centered open={modal} onCancel={() => setModal(false)} wrapClassName={styles.modal } 
+        title="Créer un compte"
+        >
+        <Space direction='vertical' className='w-100 text-center' size={12}>
+          <p>Veuillez renseigner vos information personnelle</p>
+          <Input placeholder='Nom'></Input>
+          <Input placeholder='Prénom'></Input>
+          <Input placeholder='Email'></Input>
+          <Input placeholder='Mot de passe'></Input>
+          <Button type='default' size='large' onClick={ () => réinitialisationMDP()} >Valider</Button>
+          { modalOpen ? mdpOublie() : creationCompte() }
+        </Space>
+        </Modal> */}
     </>
   )
 }
