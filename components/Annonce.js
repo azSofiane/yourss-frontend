860ba413b6af014ddf593e12a4040d78f4f92f03
WinDey91 <108035290+WinDey91@users.noreply.github.com>
2023-08-04 11:29:50 +0200

    modification annonce connection front back

--- components/Annonce.js
+++ components/Annonce.js
@@ -14,12 +14,55 @@ import {
 } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import dayjs from "dayjs";
 
 const { RangePicker } = DatePicker;
 const { TextArea } = Input;
 
 function Annonce() {
-  const test = false;
+  const test = true;
+
+  const[creationTitre,setCreationTitre] = useState("");
+  const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY', 'DD-MM-YYYY', 'DD-MM-YY'];
+  const[creationDateDebut,setCreationDateDebut] = useState("");
+  const[creationDateFin,setCreationDateFin] = useState(null);
+  const[creationPoste,setCreationPoste] = useState("");
+  const[creationEntreprise,setCreationEntreprise] = useState("");
+  const[creationDescription,setCreationDescription] = useState("");
+  const[creationAdresse,setCreationAdresse] = useState("");
+  const[creationCodePostal,setCreationCodePostal] = useState("");
+  const[creationVille,setCreationVille] = useState("");
+  const[creationDatePublication,setCreationDatePublication] = useState("");
+  const[creationDateCreation, setCreationDateCreation] = useState(new Date()); // si la date a changé, permet de garder en mémoire.
+
+
+  // fonction connexion utilisateur
+  // Champs obligatoire : ['titre', 'date_de_creation', 'code_postal', 'ville', 'description', 'token' ]
+  const handleCreationAnnonce = () => {
+    fetch('http://localhost:3000/annonces/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ titre:creationTitre, date_de_debut:creationDateDebut, date_de_fin: creationDateFin, profession: creationPoste, entreprise:creationEntreprise,description:creationDescription, date_de_publication: creationDatePublication, ville:creationVille,code_postal:creationCodePostal, date_de_creation: creationDateCreation, adresse:creationAdresse }),
+    }).then(response => response.json())
+      .then(data => {
+        console.log(handleCreationAnnonce)
+        // Si la connnexion est réussie et que le backend renvoie un token
+        if (data.result) {
+          // 
+          setCreationTitre('');
+          setCreationDateDebut(null);
+          setCreationDateFin(null);
+          setCreationPoste('');
+          setCreationEntreprise('');
+          setCreationDescription('');
+          setCreationAdresse('');
+          setCreationCodePostal('');
+          setCreationVille('');
+        }
+        console.log(data)
+      });
+  };
 
   return (
     <>
@@ -72,12 +115,28 @@ function Annonce() {
               <Col span={24}>
                 <Card>
                   <Row gutter={[12, 12]}>
-                    <Col span={24}>{ test ? <Input placeholder="Titre" size="large" /> : 'Titre' }</Col>
-                    <Col span={24} size="large"  ><RangePicker size="large"/></Col>
-                    <Col span={24}>{ test ? <Input placeholder="Poste" size="large" /> : 'Localisation' }</Col>
-                    <Col span={24}>{ test ? <Input placeholder="Entreprise" size="large" /> : 'Profession' }</Col>
-                    <Col span={24}> {test ? <TextArea rows={12} placeholder="Description du stage"/>: "Description du stage" } </Col>
-                    <Col span={24}><Button type='default' size='large' className='mx-2' onClick={() => fonctionachanger()}>Valider</Button></Col>
+                    <Col span={24}>{ test ? <Input placeholder="Titre" size="large" onChange={(e) => setCreationTitre(e.target.value)} value={creationTitre}/> : 'Titre' }</Col>
+
+                    <Col span={24}>{ test ? <DatePicker defaultValue={dayjs('01/01/2015', dateFormatList[0])} format={dateFormatList} size="large" placeholder="Date Publication" onChange={(date, dateString) => setCreationDateDebut(dateString)} value={creationDatePublication}/>: 'Date de publication' }</Col>
+
+                    <Col span={24}> {test ? <DatePicker defaultValue={dayjs('01/01/2015', dateFormatList[0])} format={dateFormatList} size="large" placeholder="Date Début" onChange={(date, dateString) => setCreationDateDebut(dateString)} value={creationDateDebut}/> : 'Date de début'}</Col>
+                      
+                    <Col span={24}> {test ? <DatePicker defaultValue={dayjs('01/01/2015', dateFormatList[0])} format={dateFormatList} size="large" placeholder="Date Fin" onChange={(date, dateString) => setCreationDateDebut(dateString)} value={creationDateFin} /> : 'Date de fin'}</Col>
+
+
+                    <Col span={24}>{ test ? <Input placeholder="Poste" size="large" onChange={(e) => setCreationPoste(e.target.value)} value={creationPoste}/> : 'Profession' }</Col>
+
+                    <Col span={24}>{ test ? <Input placeholder="Entreprise" size="large" onChange={(e) => setCreationEntreprise(e.target.value)} value={creationEntreprise} /> : 'Entreprise' }</Col>
+
+                    <Col span={24}>{ test ? <Input placeholder="Ville" size="large" onChange={(e) => setCreationVille(e.target.value)} value={creationVille} /> : 'Ville' }</Col>
+
+                    <Col span={24}>{ test ? <Input placeholder="Adresse" size="large" onChange={(e) => setCreationAdresse(e.target.value)} value={creationAdresse} /> : 'Adresse' }</Col>
+
+                    <Col span={24}>{ test ? <Input placeholder="Code postal" size="large" onChange={(e) => setCreationCodePostal(e.target.value)} value={creationCodePostal} /> : 'Code postal' }</Col>
+
+                    <Col span={24}> {test ? <TextArea rows={12} placeholder="Description du stage" onChange={(e) => setCreationDescription(e.target.value)} value={creationDescription} />: "Description du stage" } </Col>
+
+                    <Col span={24}><Button type='default' size='large' className='mx-2' onClick={() => handleCreationAnnonce()}>Valider</Button></Col>
                   </Row>
                 </Card>
               </Col>
